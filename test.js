7c863c87bca852965f5844f9fc72f7804ec3902a
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2025-09-09 02:25:25 +0630

    Add basic math function tests

---
+++ test.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+describe('Math functions', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+  });
+
+  test('subtracts 5 - 3 to equal 2', () => {
+    expect(subtract(5, 3)).toBe(2);
+  });
+});
